e3e48af8ed64f3fca105bbecb153d7275c127ba0
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-10-30 21:36:35 +0600

    Commit:Fixed style

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,7 +4,7 @@ import { IoSearch } from "react-icons/io5";
 import { LuShoppingBag } from "react-icons/lu";
 const Nav = () => {
     return (
-        <div className=' bg-red-400 w-full h-[100px] flex justify-between items-center  px-8 '>
+        <div className='  w-full h-[100px] flex justify-between items-center  px-8 '>
             <div className='w-[60px] h-[60px]  bg-white flex justify-center items-center rounded-md shadow-xl'>
                 <MdFastfood className="w-[30px] h-[30px]  text-green-500   " />
             </div>
@@ -12,7 +12,8 @@ const Nav = () => {
                 <IoSearch className=' text-green-500 w-5 h-5' />
                 <input type="text"  placeholder='Search Items ...' className=' w-full outline-none text-[20px]' />
             </form>
-            <div className=' w-[60px] h-[60px]  bg-white flex justify-center items-center rounded-md shadow-xl'>
+            <div className=' w-[60px] h-[60px]  bg-white flex justify-center items-center rounded-md shadow-xl relative  '>
+                <span className='absolute top-0 right-2 text-green-500 font-bold text-[18px]'> 0</span>
                 <LuShoppingBag className="w-[30px] h-[30px]  text-green-500" />
             </div>
         </div>
