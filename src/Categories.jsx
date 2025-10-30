bdfeb3885de9fd6ac8c1f5addee51010d07669cd
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-10-30 21:58:56 +0600

    rename Keys

--- src/Categories.jsx
+++ src/Categories.jsx
@@ -6,47 +6,49 @@ import { MdOutlineFoodBank } from "react-icons/md";
 import { GiFullPizza } from "react-icons/gi";
 import { GiHamburger } from "react-icons/gi";
 
-export const categories = [
+export const Categories = [
     {
         id: 1,
         name: "All",
-        image: <TiThSmallOutline className="w-[60px] h-[60px] text-green-500" />
+        icon: <TiThSmallOutline className="w-[60px] h-[60px] text-green-500" />
 
     },
 
     {
         id: 2,
         name: "Breakfast",
-        image: <MdOutlineFreeBreakfast className="w-[60px] h-[60px] text-green-500" />
+        icon: <MdOutlineFreeBreakfast className="w-[60px] h-[60px] text-green-500" />
 
     },
     {
         id: 3,
         name: "Soup",
-        image: <TbSoup className="w-[60px] h-[60px] text-green-500" />
+        icon: <TbSoup className="w-[60px] h-[60px] text-green-500" />
 
     },
     {
         id: 4,
         name: "Noodles",
-        image: <CiBowlNoodles className="w-[60px] h-[60px] text-green-500" />
+        icon: <CiBowlNoodles className="w-[60px] h-[60px] text-green-500" />
 
     },
     {
         id: 6,
         name: "FoodBank ",
-        image: <MdOutlineFoodBank className="w-[60px] h-[60px] text-green-500" />
+        icon: <MdOutlineFoodBank className="w-[60px] h-[60px] text-green-500" />
 
     },
     {
         id: 7,
         name: "Pizza",
-        image: <GiFullPizza className="w-[60px] h-[60px] text-green-500" />
+        icon: <GiFullPizza className="w-[60px] h-[60px] text-green-500" />
 
     }, {
         id: 8,
         name: "Pizza",
-        image: <GiHamburger className="w-[60px] h-[60px] text-green-500" />
+        icon: <GiHamburger className="w-[60px] h-[60px] text-green-500" />
     }
 
 ]
+
+export default Categories
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Nav from '../components/Nav/Nav'
+import Categories from '../Categories'
 
 const Home = () => {
   return (
-    <div className='bg-slate-200 w-full min-h-screen'>
-        
+    <div className='bg-slate-200 w-full min-h-screen'>        
         <Nav/>
+        {
+            Categories.map()
+        }
     </div>
   )
 }
