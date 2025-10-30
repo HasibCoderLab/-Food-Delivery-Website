6fe817f739c2d5114c642e8e49cdb97ff2778456
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-10-30 22:24:25 +0600

    Categories Fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,9 +6,17 @@ const Home = () => {
   return (
     <div className='bg-slate-200 w-full min-h-screen'>        
         <Nav/>
+        <div className='flex flex-wrap justify-center items-center gap-5 w-full'> 
         {
-            Categories.map()
+            Categories.map((item) =>{
+             return   <div className='w-[150px] h-[140px] bg-white flex flex-col  items-start justify-start gap-5 p-5 text-xl font-semibold text-gray-600 rounded-lg shadow-xl cursor-pointer hover:bg-green-200 transition-all duration-200'>
+                    {item.icon}
+                    {item.name}
+
+                </div>
+            })
         }
+        </div>
     </div>
   )
 }
