fe7e517632b50a226003d2bd1a2091b79a8b72c3
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-10-31 20:52:46 +0600

    btn style

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,9 +2,9 @@
     import { LuLeafyGreen } from "react-icons/lu";
 
     import image1 from "../assets/image1.avif"
-    const Card = () => {
+    const Card = ({name , image , id, price,type}) => {
         return (
-            <div className='w-[300px] h-[400px] bg-white p-3 rounded-lg flex flex-col gap-3'>
+            <div className='w-[300px] h-[400px] bg-white p-3 rounded-lg flex flex-col gap-3 shadow-lg'>
                 <div className='w-full h-[60%] overflow-hidden rounded-lg'>
                     <img src={image1} alt="pancakes" className='object-cover' />
                 </div>
@@ -18,7 +18,8 @@
                     </div>
 
                 </div>
-                <button></button>
+                <button className=' w-full rounded-lg bg-green-200 p-3 text-gray-700 hover:bg-green-400
+                transition-all cursor-pointer'>Add to dish</button>
             </div>
         )
     }
