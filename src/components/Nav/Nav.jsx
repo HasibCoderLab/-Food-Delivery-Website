9016f0d148801c4bdf4b90f3c65aa98c117194f6
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-10-31 17:18:33 +0600

    responsive

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,13 +4,13 @@ import { IoSearch } from "react-icons/io5";
 import { LuShoppingBag } from "react-icons/lu";
 const Nav = () => {
     return (
-        <div className='  w-full h-[100px] flex justify-between items-center  px-8 '>
+        <div className='  w-full h-[100px] flex justify-between items-center px-5  md:px-8 '>
             <div className='w-[60px] h-[60px]  bg-white flex justify-center items-center rounded-md shadow-xl'>
                 <MdFastfood className="w-[30px] h-[30px]  text-green-500   " />
             </div>
-            <form className=' bg-white w-[70%] h-[60px] flex  items-center  px-5  gap-5 rounded-xl shadow-md'>
+            <form className=' bg-white md:w-[70%] w-[40%] h-[60px] flex  items-center  px-5  gap-5 rounded-xl shadow-md'>
                 <IoSearch className=' text-green-500 w-5 h-5' />
-                <input type="text"  placeholder='Search Items ...' className=' w-full outline-none text-[20px]' />
+                <input type="text"  placeholder='Search Items ...' className=' w-full outline-none text-[16px] md:text-[20px]' />
             </form>
             <div className=' w-[60px] h-[60px]  bg-white flex justify-center items-center rounded-md shadow-xl relative  '>
                 <span className='absolute top-0 right-2 text-green-500 font-bold text-[18px]'> 0</span>
