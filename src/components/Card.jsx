45433623a14d3c2260a4ef77093441d24549a605
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-10-31 20:36:30 +0600

    img style

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,15 +4,16 @@ import { LuLeafyGreen } from "react-icons/lu";
 import image1 from "../assets/image1.avif"
 const Card = () => {
     return (
-        <div className='w-[280px]'>
-            <div>
-                <img src={image1} alt="pancakes" /></div>
+        <div className='w-[300px] h-[400px] bg-white p-4 rounded-lg'>
+            <div className='w-full h-[60%] overflow-hidden rounded-lg'>
+                <img src={image1} alt="pancakes" className='object-cover' />
+            </div>
             <div>pancakes </div>
-           
+
             <div>
                 <div>Tk 299/-</div>
                 <div><LuLeafyGreen /> <span> Veg</span></div>
-                
+
             </div>
         </div>
     )
