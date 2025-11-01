f1167467a4991888194f6ae0d70e6d1b347ce3eb
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-11-01 16:23:43 +0600

    add comment out SMS

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,46 +1,56 @@
-import React from 'react'
-import { LuLeafyGreen } from "react-icons/lu";
-import { GiChickenOven } from "react-icons/gi";
-import { useDispatch } from 'react-redux';
-import { AddItem } from '../redux/cartSlice';
+    import React from 'react'
+    import { LuLeafyGreen } from "react-icons/lu";
+    import { GiChickenOven } from "react-icons/gi";
+    import { useDispatch } from 'react-redux';
+    import { AddItem } from '../redux/cartSlice';
 
 
 
-const Card = ({ name, image, id, price, type }) => {
-    let disPatch =  useDispatch();
-    return (
-        <div
-            className='w-[300px] h-[400px] bg-white p-3 rounded-lg flex flex-col gap-3 shadow-lg hover:border-2 border-green-400   cursor-pointer'>
-
+    const Card = ({ name, image, id, price, type }) => {
+        let disPatch =  useDispatch();
+        return (
             <div
-                className='w-full h-[60%] overflow-hidden rounded-lg'>
-                <img src={image} alt="pancakes" className='object-cover' />
-            </div>
-
-            <div
-                className='text-2xl font-semibold'>{name} </div>
-
-            <div
-                className='w-full flex justify-between  items-center'>
-                <div
-                    className='text-lg font-bold text-green-500'>
-                    Tk {price}/-
-                </div>
+                className='w-[300px] h-[400px] bg-white p-3 rounded-lg flex flex-col gap-3 shadow-lg
+                hover:border-2 border-green-400   cursor-pointer'>
 
                 <div
-                    className=' flex justify-center items-center gap-2 text-green-500 text-xl    font-semibold' >
-                    {
-                        type === "veg" ? <LuLeafyGreen /> : <GiChickenOven />
-                    }
-                    <span> {type}</span>
+                    className='w-full h-[60%] overflow-hidden rounded-lg'>
+                    <img src={image} alt="pancakes" className='object-cover' />
                 </div>
 
-            </div>
-            <button
-                className=' w-full rounded-lg bg-green-500 p-3 text-white hover:bg-green-400
-                transition-all cursor-pointer' onClick={() => disPatch(AddItem({id:id}))} >Add to dish</button>
-        </div>
-    )
-}
+    {/* ============================= name , price ======================= */}
 
-export default Card
+                    <div
+                        className='text-2xl font-semibold'>{name} 
+                    </div>
+
+                <div
+                    className='w-full flex justify-between  items-center'>
+                    <div
+                        className='text-lg font-bold text-green-500'>
+                        Tk {price}/-
+                </div>
+
+    {/* ============================= condition fro icon ======================= */}
+                    <div
+                        className=' flex justify-center items-center gap-2 text-green-500 text-xl    
+                        font-semibold' >
+                        {
+                            type === "veg" ? <LuLeafyGreen /> : <GiChickenOven />
+                        }
+                        <span> {type}</span>
+                    </div>
+
+                </div>
+
+    {/* ============================= btn ======================= */}
+                <button
+                    className=' w-full rounded-lg bg-green-500 p-3 text-white hover:bg-green-400
+                    transition-all cursor-pointer' 
+                    onClick={() => disPatch(AddItem({id:id, name:name, price:price,image:image}))} >Add to dish
+                    </button>
+            </div>
+        )
+    }
+
+    export default Card
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,53 +1,62 @@
-import  { useContext, useEffect } from 'react'
-import { MdFastfood } from "react-icons/md";
-import { IoSearch } from "react-icons/io5";
-import { LuShoppingBag } from "react-icons/lu"
-import { food_items } from '../../food';
-import { dataContext } from '../../context/UserContext';
-import { useSelector } from 'react-redux';
-const Nav = () => {
+        import  { useContext, useEffect } from 'react'
+        import { MdFastfood } from "react-icons/md";
+        import { IoSearch } from "react-icons/io5";
+        import { LuShoppingBag } from "react-icons/lu"
+        import { food_items } from '../../food';
+        import { dataContext } from '../../context/UserContext';
+        import { useSelector } from 'react-redux';
 
 
-    const { input, setInput, cate, setCate, showCart, setShowCart } = useContext(dataContext)
-    useEffect(() => {
-        const newList = food_items.filter((item) => item.food_name.includes(input)
-            || item.food_name.toLowerCase().includes(input))
-        setCate(newList)
-    }, [input])
-    const items = useSelector(state => state.cart);
-    console.log(items);
 
+        const Nav = () => {
+        // ======== context , useEffect, Function,useSelector======================
+            const { input, setInput, cate, setCate, showCart, setShowCart } = useContext(dataContext);
 
-    return (
-        <div
-            className='  w-full h-[100px] flex justify-between items-center px-5  md:px-8 '>
-            <div
-                className='w-[60px] h-[60px]  bg-white flex justify-center items-center rounded-md shadow-xl'>
-                <MdFastfood
-                    className="w-[30px] h-[30px]  text-green-500   " />
-            </div>
+            useEffect(() => {
+                const newList = food_items.filter((item) => item.food_name.includes(input)
+                    || item.food_name.toLowerCase().includes(input));
+                setCate(newList);
+            }, [input]);
+            const items = useSelector(state => state.cart);
+            
+        // ===============================================
+            return (
+                <div
+                    className='  w-full h-[100px] flex justify-between items-center px-5  md:px-8 '>
+                    <div
+                        className='w-[60px] h-[60px]  bg-white flex justify-center items-center 
+                        rounded-md shadow-xl'>
+                        <MdFastfood
+                            className="w-[30px] h-[30px]  text-green-500   " />
+                    </div>
+        {/* ============================= form ======================= */}
+                    <form
+                        className=' bg-white md:w-[70%] w-[40%] h-[60px] flex  items-center  px-5  gap-5 
+                        rounded-xl shadow-md' onSubmit={(e) => e.preventDefault()} >
+                        <IoSearch
+                            className=' text-green-500 w-5 h-5' />
+        {/* =======  ===================== input ======================= */}
 
-            <form
-                className=' bg-white md:w-[70%] w-[40%] h-[60px] flex  items-center  px-5  gap-5 rounded-xl shadow-md' onSubmit={(e) => e.preventDefault()} >
-                <IoSearch
-                    className=' text-green-500 w-5 h-5' />
-                <input type="text" placeholder='Search Items ...'
-                    className=' w-full outline-none text-[16px] md:text-[20px]' onChange={(e) => setInput(e.target.value)} value={input} />
-            </form>
+                        <input type="text" placeholder='Search Items ...'
+                            className=' w-full outline-none text-[16px] md:text-[20px]' onChange={(e) => setInput(e.target.value)} value={input} />
+                    </form>
 
-            <div
-                className=' w-[60px] h-[60px]  bg-white flex justify-center items-center rounded-md shadow-xl relative  ' onClick={() => {
-                    setShowCart(true)
-                }}>
+    {/* ============================= span, icon ======================= */}
+                    <div
+                        className=' w-[60px] h-[60px]  bg-white flex justify-center items-center 
+                        rounded-md shadow-xl relative  ' onClick={() => {
+                            setShowCart(true)
+                        }}>
 
-                <span
-                    className='absolute top-0 right-2 text-green-500 font-bold text-[18px]'> 0
-                </span>
-                <LuShoppingBag
-                    className="w-[30px] h-[30px]  text-green-500 cursor-pointer" />
-            </div>
-        </div>
-    )
-}
+                        <span
+                            className='absolute top-0 right-2 text-green-500 font-bold text-[18px]'> 
+                            {items.length}
+                        </span>
+                        <LuShoppingBag
+                            className="w-[30px] h-[30px]  text-green-500 cursor-pointer" />
+                    </div>
+                </div>
+            )
+        }
 
-export default Nav
+        export default Nav
